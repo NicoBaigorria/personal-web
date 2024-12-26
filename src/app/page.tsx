6b02508aed800dc59a200131a9466e0f4693830f
Nicolás Baigorria <56775102+NicoBaigorria@users.noreply.github.com>
2024-12-25 23:06:45 -0600

    detalles finales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,6 +45,15 @@ export default function Page() {
 
       <p className={styles.notable}>Notable Projects</p>
       <ProjectsExamples />
+
+      <div className="light light1"></div>
+    <div className="light light2"></div>
+    <div className="light light3"></div>
+    <div className="light light4"></div>
+    <div className="light light5"></div>
+    <div className="light light6"></div>
+    <div className="light light7"></div>
+    <div className="light light8"></div>
     </div>
   );
 }
